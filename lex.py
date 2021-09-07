26b8b057a84dcc244ddedbda8e231c9aababbf34
scribble <58890963+rinzlxr@users.noreply.github.com>
2021-09-07 22:57:23 +0200

    remove the ui

--- lex.py
+++ lex.py
@@ -3,11 +3,11 @@ import os
 lines = open('./data.txt', 'r').read().split('\n')
 
 prog_txt = ""
-init_txt = ""
-func_txt = ""
-get_txt = ""
-local_txt = ""
-kill_txt = ""
+init_txt = "" # constructor
+func_txt = "" # functions
+get_txt = "" # get an item
+local_txt = "" 
+kill_txt = "" # closeout
 
 for line in lines:
     if line.find(':') > 0:
@@ -28,30 +28,30 @@ for line in lines:
                 'reqs': items
             }
             fn = f"has_{item['name'].replace(' ', '_')}_reqs"
-            kill_txt += f"\t\tApp.globalPut(Bytes('{item['name']}'), App.globalGet(Bytes('{item['name']}')) + App.localGet(Int(0), Bytes('{item['name']}'))),\n"
-            init_txt += f"\t\tApp.globalPut(Bytes('{item['name']}'), Int(0)),\n"
-            local_txt += f"\t\tApp.localPut(Int(0), Bytes('{item['name']}'), Int(0)),\n"
-            func_txt += f"\t{fn} = Seq([\n"
-            ask_func_txt = ""#\t\tIf(And(\n"
+            kill_txt += f"  App.globalPut(Bytes('{item['name']}'), App.globalGet(Bytes('{item['name']}')) + App.localGet(Int(0), Bytes('{item['name']}'))),\n"
+            init_txt += f"  App.globalPut(Bytes('{item['name']}'), Int(0)),\n"
+            local_txt += f"  App.localPut(Int(0), Bytes('{item['name']}'), Int(0)),\n"
+            func_txt += f" {fn} = Seq([\n"
+            ask_func_txt = ""#  If(And(\n"
             ch_func_txt = ""
             counter = 0
             for i in item['reqs']:
                 char = ""
                 if counter == len(item['reqs']) - 1: 
                     char = ")"
-                ask_func_txt += f"\t\tAssert(App.localGet(Int(0), Bytes('{i['name']}')) >= (Int({i['items']}) * amount)),\n"
-                ch_func_txt += f"\t\tApp.localPut(Int(0), Bytes('{i['name']}'), App.localGet(Int(0), Bytes('{i['name']}')) - (Int({i['items']}) * amount)),\n"
+                ask_func_txt += f"  Assert(App.localGet(Int(0), Bytes('{i['name']}')) >= (Int({i['items']}) * amount)),\n"
+                ch_func_txt += f"  App.localPut(Int(0), Bytes('{i['name']}'), App.localGet(Int(0), Bytes('{i['name']}')) - (Int({i['items']}) * amount)),\n"
                 counter += 1
-            #ask_func_txt += "\t\t\tReturn(Int(1))\n"
-            #ask_func_txt += "\t\t),\n"
+            #ask_func_txt += "   Return(Int(1))\n"
+            #ask_func_txt += "  ),\n"
             
             func_txt += ask_func_txt
             func_txt += ch_func_txt
 
-            func_txt += f"\t\tApp.localPut(Int(0), Bytes('{item['name']}'), App.localGet(Int(0), Bytes('{item['name']}')) + (Int(1) * amount)),\n"
-            func_txt += f"\t\tReturn(Int(1))\n"
-            func_txt += f"\t])\n\n"
-            prog_txt += f"\t\t[Txn.application_args[0] == Bytes('craft {item['name']}'), {fn}],\n"
+            func_txt += f"  App.localPut(Int(0), Bytes('{item['name']}'), App.localGet(Int(0), Bytes('{item['name']}')) + (Int(1) * amount)),\n"
+            func_txt += f"  Return(Int(1))\n"
+            func_txt += f" ])\n\n"
+            prog_txt += f"  [Txn.application_args[0] == Bytes('craft {item['name']}'), {fn}],\n"
     else:
         if len(line) <= 0:
             break
@@ -61,53 +61,53 @@ for line in lines:
             'count': key[1]
         }
 
-        kill_txt += f"\t\tApp.globalPut(Bytes('{item['name']}'), App.globalGet(Bytes('{item['name']}')) + App.localGet(Int(0), Bytes('{item['name']}'))),\n"
-        init_txt += f"\t\tApp.globalPut(Bytes('{item['name']}'), Int({item['count']})),\n"
-        local_txt += f"\t\tApp.localPut(Int(0), Bytes('{item['name']}'), Int(0)),\n"
+        kill_txt += f"  App.globalPut(Bytes('{item['name']}'), App.globalGet(Bytes('{item['name']}')) + App.localGet(Int(0), Bytes('{item['name']}'))),\n"
+        init_txt += f"  App.globalPut(Bytes('{item['name']}'), Int({item['count']})),\n"
+        local_txt += f"  App.localPut(Int(0), Bytes('{item['name']}'), Int(0)),\n"
 
 print("from pyteal import *\n")
 print("def approval():")
-print("\ton_init = Seq([")
-print("\t\tApp.globalPut(Bytes('admin'), Txn.sender()),")
+print(" on_init = Seq([")
+print("  App.globalPut(Bytes('admin'), Txn.sender()),")
 print(init_txt)
-print("\t\tReturn(Int(1))")
-print("\t])\n")
+print("  Return(Int(1))")
+print(" ])\n")
 
-print("\ton_join = Seq([")
+print(" on_join = Seq([")
 print(local_txt)
-print("\t\tReturn(Int(1))")
-print("\t])\n")
+print("  Return(Int(1))")
+print(" ])\n")
 
-print("\ton_leave = Seq([")
-print("\t\tReturn(Int(1))")
-print("\t])\n")
+print(" on_leave = Seq([")
+print("  Return(Int(1))")
+print(" ])\n")
 
-print("\tis_creator = Txn.sender() == App.globalGet(Bytes('admin'))\n")
-print("\tamount = Btoi(Txn.application_args[1]) or 1")
+print(" is_creator = Txn.sender() == App.globalGet(Bytes('admin'))\n")
+print(" amount = Btoi(Txn.application_args[1]) or 1")
 
 print(func_txt)
 
-print(f"\tname = Txn.application_args[1]")
-print(f"\titem = App.globalGet(name)")
-print(f"\tamount = Btoi(Txn.application_args[2])")
-get_txt += f"\ton_get = Seq([\n"
-get_txt += f"\t\tAssert(item >= amount),\n"
-get_txt += f"\t\tAssert(item >= Int(0)),\n"
-get_txt += f"\t\tApp.globalPut(name, item - amount),\n"
-get_txt += f"\t\tApp.localPut(Int(0), name, App.localGet(Int(0), name) + amount),\n"
-get_txt += f"\t\tReturn(Int(1)),\n"
-get_txt += "\t])\n\n"
+print(f" name = Txn.application_args[1]")
+print(f" item = App.globalGet(name)")
+print(f" amount = Btoi(Txn.application_args[2])")
+get_txt += f" on_get = Seq([\n"
+get_txt += f"  Assert(item >= amount),\n"
+get_txt += f"  Assert(item >= Int(0)),\n"
+get_txt += f"  App.globalPut(name, item - amount),\n"
+get_txt += f"  App.localPut(Int(0), name, App.localGet(Int(0), name) + amount),\n"
+get_txt += f"  Return(Int(1)),\n"
+get_txt += " ])\n\n"
 print(get_txt)
 
-print("\treturn Cond(")
-print("\t\t[Txn.application_id() == Int(0), on_init],")
-print("\t\t[Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],")
-print("\t\t[Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],")
-print("\t\t[Txn.on_completion() == OnComplete.OptIn, on_join],")
-print("\t\t[Txn.on_completion() == OnComplete.CloseOut, on_leave],")
-prog_txt += f"\t\t[Txn.application_args[0] == Bytes('get'), on_get],\n"
+print(" return Cond(")
+print("  [Txn.application_id() == Int(0), on_init],")
+print("  [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],")
+print("  [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],")
+print("  [Txn.on_completion() == OnComplete.OptIn, on_join],")
+print("  [Txn.on_completion() == OnComplete.CloseOut, on_leave],")
+prog_txt += f"  [Txn.application_args[0] == Bytes('get'), on_get],\n"
 print(prog_txt)
-print("\t)\n")
+print(" )\n")
 
 print("def clear():")
 print("    return Seq([Return(Int(1))])\n")
